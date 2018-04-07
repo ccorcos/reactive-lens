0fa71d9baa5cf8b85ff31c7d61e53d9861831a39
Chet Corcos <ccorcos@gmail.com>
2018-04-07 12:07:19 -0700

    First

--- App.tsx
+++ App.tsx
@@ -1,25 +1,98 @@
 import * as React from "react"
-import { Value } from "reactive-magic"
-import Component from "reactive-magic/component"
 
-export default class App extends Component {
-	count = new Value(0)
+class Dependency {
+	private listeners: Set<() => void> = new Set()
+	public add(listener: () => void) {
+		this.listeners.add(listener)
+	}
+	public remove(listener: () => void) {
+		this.listeners.delete(listener)
+	}
+	// Emit an event and prevent circular dependencies.
+	private emitting = false
+	public emit = () => {
+		if (this.emitting) {
+			return
+		}
+		this.emitting = true
+		this.listeners.forEach(listener => listener())
+		this.emitting = false
+	}
+}
 
-	increment = () => {
-		this.count.update(count => count + 1)
+const contexts: Array<Set<Dependency>> = []
+
+class Lens<State> {
+	constructor(private state: State) {}
+	private dependency = new Dependency()
+	get(): State {
+		const context = contexts[0]
+		context && context.add(this.dependency)
+		return this.state
+	}
+	set(state: State) {
+		this.dependency.emit()
+		this.state = state
+	}
+	prop(key: keyof State) {
+		return new Lens(this.state[key])
+	}
+}
+
+interface AppState {
+	count: number
+	delta: number
+}
+
+const appState = new Lens<AppState>({ count: 0, delta: 1 })
+
+interface CounterProps {
+	count: Lens<number>
+}
+
+export default class Counter extends React.Component<CounterProps> {
+	private increment = () => {
+		this.props.count.set(this.props.count.get() + 1)
 	}
 
-	decrement = () => {
-		this.count.update(count => count - 1)
+	private decrement = () => {
+		this.props.count.set(this.props.count.get() - 1)
 	}
 
-	view() {
-		return (
-			<div>
-				<button onClick={this.decrement}>{"-"}</button>
-				<span>{this.count.get()}</span>
-				<button onClick={this.increment}>{"+"}</button>
-			</div>
-		)
+	componentWillUnmount() {
+		this.cleanup()
+	}
+
+	private dependencies = new Set<Dependency>()
+
+	private update = () => this.forceUpdate()
+
+	private cleanup() {
+		this.dependencies.forEach(dependency => {
+			dependency.remove(this.update)
+		})
+	}
+
+	private reactive<T>(fn: () => T): T {
+		this.cleanup()
+		contexts.push(this.dependencies)
+		const result = fn()
+		this.dependencies.forEach(dependency => {
+			dependency.add(this.update)
+		})
+		contexts.pop()
+		return result
+	}
+
+	render() {
+		return this.reactive(() => {
+			return (
+				<div>
+					<button onClick={this.decrement}>{"-"}</button>
+					<span>{this.props.count.get()}</span>
+					<button onClick={this.increment}>{"+"}</button>
+				</div>
+			)
+		})
 	}
 }
--- README.md
+++ README.md
@@ -1,11 +1,5 @@
-# Parcel Boilerplate
+# Reactive Lenses
 
-This is a minimal boilerplate application for building web applications.
+The goal of this project is to combine the benefits of single-atom state with the convenience of reactive variables.
 
-## Features
-
-- React
-- Reactive Magic
-- Parcel
-- Typescript
-- Glamor
+The idea is to uses lenses to query different parts of the global state and compute updates based on all downstream lenses.
