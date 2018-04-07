f1e8d896a1578d73be4cd911fb2caa416dbb9a90
Chet Corcos <ccorcos@gmail.com>
2018-04-07 12:36:22 -0700

    It works!

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import * as _ from "lodash"
 
 class Dependency {
 	private listeners: Set<() => void> = new Set()
@@ -23,19 +24,113 @@ class Dependency {
 const contexts: Array<Set<Dependency>> = []
 
 class Lens<State> {
-	constructor(private state: State) {}
+	constructor(private parent: { get(): State; set(newState: State): void }) {}
 	private dependency = new Dependency()
 	get(): State {
 		const context = contexts[0]
 		context && context.add(this.dependency)
-		return this.state
+		return this.parent.get()
 	}
-	set(state: State) {
+	set(newState: State) {
 		this.dependency.emit()
-		this.state = state
+		this.parent.set(newState)
 	}
+	private cache = new Map<number | string, Lens<any>>()
 	prop(key: keyof State) {
-		return new Lens(this.state[key])
+		const cached = this.cache.get(key)
+		if (cached) {
+			return cached
+		}
+		const newLens = new Lens({
+			get: () => this.parent.get()[key],
+			set: newState => {
+				const parentState = this.parent.get()
+				if (_.isPlainObject(parentState)) {
+					const newObject = {
+						...(parentState as any), // TODO: better types
+						[key]: newState,
+					}
+					this.parent.set(newObject)
+				} else if (_.isArray(parentState)) {
+					const newArray = [...parentState]
+					newArray[key as any] = newState // TODO: better types
+					this.parent.set(newArray as any) // TODO: better types
+				} else {
+					throw new Error("This shouldn't be possible.")
+				}
+			},
+		})
+		this.cache.set(key, newLens)
+		return newLens
+	}
+}
+
+function Reactive<T extends { new (...args: any[]): React.Component }>(
+	constructor: T
+) {
+	return class extends constructor {
+		componentWillUnmount() {
+			this.cleanup()
+			if (constructor.prototype.componentWillUnmount) {
+				constructor.prototype.componentWillUnmount.call(this)
+			}
+		}
+
+		private dependencies = new Set<Dependency>()
+
+		private update = () => this.forceUpdate()
+
+		private cleanup() {
+			this.dependencies.forEach(dependency => {
+				dependency.remove(this.update)
+			})
+		}
+
+		private reactive<T>(fn: () => T): T {
+			this.cleanup()
+			contexts.push(this.dependencies)
+			const result = fn()
+			this.dependencies.forEach(dependency => {
+				dependency.add(this.update)
+			})
+			contexts.pop()
+			return result
+		}
+
+		render() {
+			return this.reactive(() => {
+				return constructor.prototype.render.call(this)
+			})
+		}
+	}
+}
+
+interface CounterProps {
+	label: string
+	count: Lens<number>
+	delta: number
+}
+
+@Reactive
+class Counter extends React.Component<CounterProps> {
+	private increment = () => {
+		this.props.count.set(this.props.count.get() + this.props.delta)
+	}
+
+	private decrement = () => {
+		this.props.count.set(this.props.count.get() - this.props.delta)
+	}
+
+	render() {
+		console.log("Counter", this.props)
+		return (
+			<div>
+				<span>{this.props.label}</span>
+				<button onClick={this.decrement}>{"-"}</button>
+				<span>{this.props.count.get()}</span>
+				<button onClick={this.increment}>{"+"}</button>
+			</div>
+		)
 	}
 }
 
@@ -44,55 +139,27 @@ interface AppState {
 	delta: number
 }
 
-const appState = new Lens<AppState>({ count: 0, delta: 1 })
+let _appState = { count: 0, delta: 1 }
+const appState = new Lens<AppState>({
+	get: () => _appState,
+	set: newState => (_appState = newState),
+})
 
-interface CounterProps {
-	count: Lens<number>
-}
-
-export default class Counter extends React.Component<CounterProps> {
-	private increment = () => {
-		this.props.count.set(this.props.count.get() + 1)
-	}
-
-	private decrement = () => {
-		this.props.count.set(this.props.count.get() - 1)
-	}
-
-	componentWillUnmount() {
-		this.cleanup()
-	}
-
-	private dependencies = new Set<Dependency>()
-
-	private update = () => this.forceUpdate()
-
-	private cleanup() {
-		this.dependencies.forEach(dependency => {
-			dependency.remove(this.update)
-		})
-	}
-
-	private reactive<T>(fn: () => T): T {
-		this.cleanup()
-		contexts.push(this.dependencies)
-		const result = fn()
-		this.dependencies.forEach(dependency => {
-			dependency.add(this.update)
-		})
-		contexts.pop()
-		return result
-	}
-
+@Reactive
+class App extends React.Component {
 	render() {
-		return this.reactive(() => {
-			return (
-				<div>
-					<button onClick={this.decrement}>{"-"}</button>
-					<span>{this.props.count.get()}</span>
-					<button onClick={this.increment}>{"+"}</button>
-				</div>
-			)
-		})
+		console.log("App")
+		return (
+			<div>
+				<Counter
+					label={"count"}
+					count={appState.prop("count")}
+					delta={appState.prop("delta").get()}
+				/>
+				<Counter label={"delta"} count={appState.prop("delta")} delta={1} />
+			</div>
+		)
 	}
 }
+
+export default App
